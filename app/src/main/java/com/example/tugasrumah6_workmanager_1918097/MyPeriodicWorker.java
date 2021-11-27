d86fad01b3ea18ec4303cc06d442857a31734e9a
muhammadhasansyuaibi <muhammadhasansyuaibi@gmail.com>
2021-11-27 22:49:53 +0700

    Periodic Work Request 1918097

---
+++ app/src/main/java/com/example/tugasrumah6_workmanager_1918097/MyPeriodicWorker.java
@@ -0,0 +1,42 @@
+package com.example.tugasrumah6_workmanager_1918097;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyPeriodicWorker extends Worker {
+
+    public MyPeriodicWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        displayNotification("Task","doWork: Work is Done");
+        return Result.success();
+    }
+
+    private void displayNotification(String task, String desc) {
+        NotificationManager manager = (NotificationManager)
+                        getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("1918097",
+                "hasby", NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(getApplicationContext(),
+                "1918097")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        manager.notify(1, builder.build());
+    }
+}
